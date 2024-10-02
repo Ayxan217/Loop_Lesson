86f82422295e08ad1e9de177f45a88827ecec094
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-02 21:46:04 +0400

    c# tasks added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿// verilmis 2 reqemli ededin en böyük reqemini tapan program
+
+//Console.WriteLine("2 reqemli eded daxil edin:");
+//int num = Convert.ToInt16(Console.ReadLine());
+//int FirstDigit = num / 10;
+//int SecondDigit = num % 10;
+
+//if (num > 99 || num<10)
+//{
+//    Console.WriteLine("düzgün eded daxil edin");
+//}
+
+// else if (FirstDigit > SecondDigit)
+//{
+//    Console.WriteLine(FirstDigit);
+//}
+//else if (SecondDigit > FirstDigit)
+//{ 
+//    Console.WriteLine(SecondDigit); 
+//}
+//else
+//{
+//Console.WriteLine("beraberdir");
+//}
+
+
+
+// ededin sade ve ya mürekkeb oldugunu tapan program
+
+
+//Console.WriteLine("eded daxil edin:");
+//int num = Convert.ToInt32(Console.ReadLine());
+//if (num <= 1) { Console.WriteLine("ne sade ne mürekkeb ededdir"); }
+
+//for(int i=2; i<num; i++)
+//{ if(num % i != 0)
+//    {
+//        Console.WriteLine("sade ededdir");
+//        break;
+
+//    }
+//    else {
+//        Console.WriteLine("murekkeb ededdir");
+//        break;
+
+
+
+//}
+//}
+
+
+
+// 2 nin qüvveti olub olmadıgını yoxlayan program
+
+
+
+//Console.WriteLine("Ededi daxil edin:");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if (num <= 0)
+//{
+//    Console.WriteLine("qüvveti deyil");
+//}
+//else
+//{
+//    bool isPowerOfTwo = true;
+
+    
+//    while (num > 1)
+//    {
+//        if (num % 2 != 0)
+//        {
+//            isPowerOfTwo = false;
+//            break;
+//        }
+//        num /= 2;
+//    }
+
+//    if (isPowerOfTwo)
+//    {
+//        Console.WriteLine("qüvvetidir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("qüvveti deyil");
+//    }
+//}
+
+
+
+
+
